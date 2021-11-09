295be1b269fb467aa47334b574676fe43242bd17
levin <levin@bbq.agency>
2021-11-09 18:23:45 +0200

    main_impl: added decorator to choose connection

--- tap_mongodb/__init__.py
+++ tap_mongodb/__init__.py
@@ -1,28 +1,51 @@
 #!/usr/bin/env python3
+import json
+import sys
+from pathlib import Path
+
+import singer
+from singer import metadata, utils
+from sshtunnel import SSHTunnelForwarder
+
+import tap_mongodb.sync_strategies.common as common
+import tap_mongodb.sync_strategies.full_table as full_table
+import tap_mongodb.sync_strategies.incremental as incremental
+import tap_mongodb.sync_strategies.oplog as oplog
 from tap_mongodb.connection import get_client
 from tap_mongodb.discover import do_discover
 from tap_mongodb.sync import do_sync, get_streams_to_sync
 from tap_mongodb.utils import get_full_catalog
-import tap_mongodb.sync_strategies.common as common
-import tap_mongodb.sync_strategies.full_table as full_table
-import tap_mongodb.sync_strategies.oplog as oplog
-import tap_mongodb.sync_strategies.incremental as incremental
-import singer
-from singer import utils, metadata
-import json
-import sys
 
 LOGGER = singer.get_logger()
 ARGS = utils.parse_args([])
 CONFIG = ARGS.config
 
 
+def choose_connection_type(func):
+    def choose():
+        if CONFIG.ssh == "true":
+            ssh_params = dict(
+                ssh_address_or_host=CONFIG.ssh_host,
+                ssh_username=CONFIG.ssh_username,
+                ssh_pkey=(str(Path.home().joinpath(".ssh").joinpath("id_rsa"))),
+                remote_bind_address=(CONFIG.host, 27017),
+            )
+            with SSHTunnelForwarder(**ssh_params):
+                func()
+        else:
+            func()
+
+    return choose
+
+
+@choose_connection_type
 def main_impl():
     client = get_client(ARGS, CONFIG)
     args = client.args
     config = client.config
-    common.INCLUDE_SCHEMAS_IN_DESTINATION_STREAM_NAME = \
-        (config.get('include_schemas_in_destination_stream_name') == 'true')
+    common.INCLUDE_SCHEMAS_IN_DESTINATION_STREAM_NAME = (
+        config.get("include_schemas_in_destination_stream_name") == "true"
+    )
 
     if args.discover:
         catalog = do_discover(client, config, limit=1000)
@@ -32,13 +55,17 @@ def main_impl():
         catalog = args.catalog.to_dict()
         # merge dictionaries to get selected streams and replication method
         # full table columns coverage && split parent-child
-        config['filter_collections'] = get_collections_to_rediscover(catalog, config.get("filter_collections", []))
+        config["filter_collections"] = get_collections_to_rediscover(
+            catalog, config.get("filter_collections", [])
+        )
         rediscovered_catalog = do_discover(client, config, limit=None)
         full_catalog = catalog
         selected_streams = None
         try:
             # some overwrite of selected=True in parent-child. Pass stream_names to restrict
-            full_catalog, selected_streams = get_full_catalog(catalog, rediscovered_catalog)
+            full_catalog, selected_streams = get_full_catalog(
+                catalog, rediscovered_catalog
+            )
         except Exception as e:
             LOGGER.info(e)
             pass
@@ -47,8 +74,8 @@ def main_impl():
 
 def get_collections_to_rediscover(catalog, default_filtered_collections):
     filtered_collections = set()
-    for stream in get_streams_to_sync(catalog['streams'], {}):
-        collection = metadata.get(metadata.to_map(stream['metadata']), (), 'collection')
+    for stream in get_streams_to_sync(catalog["streams"], {}):
+        collection = metadata.get(metadata.to_map(stream["metadata"]), (), "collection")
         if not collection:
             return default_filtered_collections  # in order to support older catalogs
         else:
--- tap_mongodb/connection.py
+++ tap_mongodb/connection.py
@@ -1,75 +1,84 @@
 #!/usr/bin/env python3
 import ssl
+from pathlib import Path
+from typing import Dict, Union
+
 import pymongo
 import singer
 from singer import utils
 
 LOGGER = singer.get_logger()
 
-REQUIRED_CONFIG_KEYS = [
-    'host',
-    'port',
-    'user',
-    'password',
-    'database'
-]
+REQUIRED_CONFIG_KEYS = ["host", "port", "user", "password", "database"]
 
 
-def get_client(args, config):
+def get_client(args, config) -> pymongo.MongoClient:
     try:
         return check_connection(args, config)
     except Exception as e:
         raise Exception("Could not connect to client! ", e)
 
 
+def create_client(
+    connection_params: Union[Dict, str], connection_args: Dict, connection_config: Dict
+) -> pymongo.MongoClient:
+    if isinstance(connection_params, dict):
+        client = pymongo.MongoClient(**connection_params)
+        LOGGER.info(
+            "Connected to MongoDB host: %s, version: %s",
+            connection_config["host"],
+            client.server_info().get("version", "unknown"),
+        )
+    elif isinstance(connection_params, str):
+        client = pymongo.MongoClient(connection_config["connection_uri"])
+        LOGGER.info(
+            "Connected to MongoDB host: %s, version: %s",
+            connection_config["connection_uri"].replace(
+                connection_config.get("password"), "********"
+            ),
+            client.server_info().get("version", "unknown"),
+        )
+    client.args = connection_args
+    client.config = connection_config
+    return client
+
+
 def check_connection(args, config):
     # self.session = pymongo.MongoClient.start_session(causal_consistency=True)
-    if 'connection_uri' in config.keys():
+    if "connection_uri" in config.keys():
         # Connect using the DNS string
-        parsed_uri = pymongo.uri_parser.parse_uri(config['connection_uri'])
-        config.update({
-            "username": parsed_uri['username'],
-            "password": parsed_uri['password'],
-            "authSource": parsed_uri['options']['authsource'],
-            "user": parsed_uri['username'],
-            "database": parsed_uri.get('database', 'admin')
-        })
-
-        client = pymongo.MongoClient(config['connection_uri'])
-        LOGGER.info('Connected to MongoDB host: %s, version: %s',
-                    config['connection_uri'].replace(config.get("password"), "********"),
-                    client.server_info().get('version', 'unknown'))
-        client.args = args
-        client.config = config
-        return client
+        parsed_uri: Dict = pymongo.uri_parser.parse_uri(config["connection_uri"])
+        config.update(
+            {
+                "username": parsed_uri["username"],
+                "password": parsed_uri["password"],
+                "authSource": parsed_uri["options"]["authsource"],
+                "user": parsed_uri["username"],
+                "database": parsed_uri.get("database", "admin"),
+            }
+        )
+        return create_client(config["connection_uri"])
     else:
         # Connect using the connection parameters
         args = utils.parse_args(REQUIRED_CONFIG_KEYS)
         config = args.config
 
         # Default SSL verify mode to true, give option to disable
-        verify_mode = config.get('verify_mode', 'true') == 'true'
-        use_ssl = config.get('ssl') == 'true'
+        verify_mode = config.get("verify_mode", "true") == "true"
+        use_ssl = config.get("ssl") == "true"
 
-        connection_params = {"host": config['host'],
-                             "port": int(config['port']),
-                             "username": config.get('user', None),
-                             "password": config.get('password', None),
-                             "authSource": config['database'],
-                             "ssl": use_ssl,
-                             "replicaset": config.get('replica_set', None),
-                             "readPreference": 'secondaryPreferred'}
+        connection_params = {
+            "host": config["host"],
+            "port": int(config["port"]),
+            "username": config.get("user", None),
+            "password": config.get("password", None),
+            "authSource": config["database"],
+            "ssl": use_ssl,
+            "replicaset": config.get("replica_set", None),
+            "readPreference": "secondaryPreferred",
+        }
 
         # NB: "ssl_cert_reqs" must ONLY be supplied if `SSL` is true.
         if not verify_mode and use_ssl:
             connection_params["ssl_cert_reqs"] = ssl.CERT_NONE
-        client = pymongo.MongoClient(**connection_params)
-
-        LOGGER.info('Connected to MongoDB host: %s, version: %s',
-                    config['host'],
-                    client.server_info().get('version', 'unknown'))
-        client.args = args
-        client.config = config
-        return client
-
-
+        return create_client(connection_params, args, config)
